1e7f509d1e98a1aaf83079510f2ba2c78baec2b3
Rishabh Sharma <rishabhsharma@Rishabhs-MacBook-Pro.local>
2018-09-14 23:33:09 -0400

    Updating;

--- src/components/CarPanel.js
+++ src/components/CarPanel.js
@@ -4,11 +4,10 @@ import React, { Component} from 'react'
 // name 
 // make 
 // model and availability 
-
 class CarPanel extends Component { 
     render(){ 
         return(
-            <div> 
+            <div>
                 <p> Name: {this.props.name} </p>
                 <p> Make: {this.props.make} </p> 
                 <p> Model: {this.props.model}</p> 
